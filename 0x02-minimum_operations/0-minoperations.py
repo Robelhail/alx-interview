b3b0aea529a812cdff1efbf4d9ef8a46e101df32
Robelhail <richnrob.2020@gmail.com>
2023-10-10 17:54:07 +0000

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,16 +7,6 @@ def minOperations(n):
     """Calculates the fewest number of operations needed to result in
     exactly n H characters in the file.
 
-    In a text file, there is a single character H. Your text editor can
-    execute only two operations in this file: Copy All and Paste characters
-    in the file.
-
-    Args:
-        n (int): The number of desired H characters.
-
-    Returns:
-        int: The number of minimal operations needed to get n H characters
-    or 0 if it is impossible to achieve n.
     """
     if not isinstance(n, int):
         return 0
